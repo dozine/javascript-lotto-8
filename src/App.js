e63e2b32c0333bcb09920c31f86ef77f92655597
dozin <dojin0345@gmail.com>
2025-11-03 22:26:19 +0900

    feat App에 예외 처리 로직 추가

--- src/App.js
+++ src/App.js
@@ -2,35 +2,42 @@ import InputView from "./InputView.js";
 import OutputView from "./OutputView.js";
 import LottoMaker from "./LottoMaker.js";
 import WinningResult from "./WinningResult.js";
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
   async run() {
-    const inputView = new InputView();
-    const purchaseAmount = await inputView.purchaseAmountInput();
-    const count = Math.floor(purchaseAmount / 1000);
+    try {
+      const inputView = new InputView();
 
-    OutputView.printPurchaseResult(count);
+      const purchaseAmount = await inputView.purchaseAmountInput();
+      const count = Math.floor(purchaseAmount / 1000);
 
-    const lottos = [];
-    for (let i = 0; i < count; i++) {
-      const lotto = LottoMaker.createLottoNumbers();
-      lottos.push(lotto);
-      OutputView.printLottoNumbers(lotto);
+      OutputView.printPurchaseResult(count);
+
+      const lottos = [];
+      for (let i = 0; i < count; i++) {
+        const lotto = LottoMaker.createLottoNumbers();
+        lottos.push(lotto);
+        OutputView.printLottoNumbers(lotto);
+      }
+
+      const winningNumbers = await inputView.winningNumbersInput();
+      const bonusNumber = await inputView.bonusNumberInput();
+
+      const result = new WinningResult();
+      for (const lotto of lottos) {
+        result.checkWinning(lotto, winningNumbers, bonusNumber);
+      }
+
+      const totalPrize = result.calculateTotalPrize();
+      OutputView.printStatistics(
+        result.getStatistics(),
+        totalPrize,
+        purchaseAmount
+      );
+    } catch (error) {
+      Console.print(error.message);
     }
-
-    const winningNumbers = await inputView.winningNumbersInput();
-    const bonusNumber = await inputView.bonusNumberInput();
-
-    const result = new WinningResult();
-    for (const lotto of lottos) {
-      result.checkWinning(lotto, winningNumbers, bonusNumber);
-    }
-
-    const totalPrize = result.calculateTotalPrize();
-    OutputView.printStatistics(
-      result.getStatistics(),
-      totalPrize,
-      purchaseAmount
-    );
   }
 }
 
