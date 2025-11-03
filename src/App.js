fcfe2c76254630b5ee257b7b209a6eb8c3fc29ac
dozin <dojin0345@gmail.com>
2025-11-03 17:35:58 +0900

    fix : 배열 타입 문제 수정
    statistics 조건 불일치 문제, cnt==6부터 해서 해결

--- src/App.js
+++ src/App.js
@@ -2,16 +2,29 @@ import { Console, Random } from "@woowacourse/mission-utils";
 class App {
   async run() {
     const purchaseAmount = await Console.readLineAsync(
-      "구입 금액을 입력해 주세요: "
+      "구입 금액을 입력해 주세요.\n"
     );
     const count = Math.floor(Number(purchaseAmount) / 1000);
-    Console.print(`${count}개를 구매했습니다.`);
+    Console.print(`\n${count}개를 구매했습니다.`);
+    const lottoNumbers = [];
+    for (let i = 0; i < count; i++) {
+      const randomNumbers = Random.pickUniqueNumbersInRange(1, 45, 6);
+      lottoNumbers.push(randomNumbers);
+      Console.print(`[${randomNumbers.join(", ")}]`);
+    }
+
     const winningNumbers = await Console.readLineAsync(
       "당첨 번호를 입력해주세요.: "
     );
     const bonusNumber = await Console.readLineAsync(
       "보너스 번호를 입력해주세요: "
     );
+    // 당첨 번호 및 보너스 번호 Number 배열로 변환
+    const winningNumbersArray = winningNumbers
+      .split(",")
+      .map((num) => Number(num.trim()));
+    const bonusNum = Number(bonusNumber.trim());
+
     let statistics = {
       "3개 일치": 0,
       "4개 일치": 0,
@@ -26,30 +39,29 @@ class App {
       "5개 일치, 보너스 볼 일치": 30000000,
       "6개 일치": 2000000000,
     };
-    for (let i = 0; i < count; i++) {
-      const randomNumbers = Random.pickUniqueNumbersInRange(1, 45, 6);
-      Console.print(`[${randomNumbers.join(", ")}]`);
 
-      for (const number of randomNumbers) {
-        let cnt = 0;
-        let bonusCnt = 0;
-        if (winningNumbers.includes(number)) {
+    for (const ticket of lottoNumbers) {
+      let cnt = 0;
+      let bonusCnt = 0;
+      for (const number of ticket) {
+        if (winningNumbersArray.includes(number)) {
           cnt++;
         }
-        if (bonusNumber.includes(number)) {
+        if (number === bonusNum) {
           bonusCnt++;
         }
-        if (cnt === 3) {
-          statistics["3개 일치"] += 1;
-        } else if (cnt === 4) {
-          statistics["4개 일치"] += 1;
-        } else if (cnt === 5) {
-          statistics["5개 일치"] += 1;
-        } else if (cnt === 5 && bonusCnt === 1) {
-          statistics["5개 일치, 보너스 볼 일치"] += 1;
-        } else if (cnt === 6) {
-          statistics["6개 일치"] += 1;
-        }
+      }
+      // 순서 변경 6개 일치부터 검사
+      if (cnt === 6) {
+        statistics["6개 일치"] += 1;
+      } else if (cnt === 5 && bonusCnt === 1) {
+        statistics["5개 일치, 보너스 볼 일치"] += 1;
+      } else if (cnt === 5) {
+        statistics["5개 일치"] += 1;
+      } else if (cnt === 4) {
+        statistics["4개 일치"] += 1;
+      } else if (cnt === 3) {
+        statistics["3개 일치"] += 1;
       }
     }
 
